081d83958c45454ab1d0d43ae81e44dc4f3791ff
naderkhalil <nader.khalil@gmail.com>
2021-03-16 10:44:58 -0700

    0.1.1

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -2,8 +2,8 @@ class Brev < Formula
   desc "Brev CLI"
   homepage "https://brev.dev"
   url "https://github.com/brevdev/brev-go-cli/releases/latest/download/brev-osx-64.tar.gz"
-  sha256 "abb8525782a0baa5fb416cbb84ca998b23cbcc6c25e8bb323045a717a194dff6"
-  version "0.1.0"
+  sha256 "5df4af103289f259c839ec09fb969035fd0e8e40362097caf6aad45b8b1293fd"
+  version "0.1.1"
 
   def install
     bin.install "brev"
