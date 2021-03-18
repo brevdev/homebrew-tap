28894c636263d0e6dc899f9ca403bf5188ebd6c1
Drew Malin <drewmalin@gmail.com>
2021-03-18 12:19:44 -0700

    completions

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -7,8 +7,8 @@ class Brev < Formula
 
   def install
     bin.install "brev"
-    system "brev" "completion" "bash" ">" "brev"
-    system "brev" "completion" "zsh" ">" "_brev"
+    system "brev", "completion", "bash", ">", "brev"
+    system "brev", "completion", "zsh", ">", "_brev"
     bash_completion.install "brev"
     zsh_completion.install "_brev"
   end
