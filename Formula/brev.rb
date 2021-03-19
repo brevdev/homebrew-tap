c8884aaa8ff142af6928344c83981a216c781b8f
Drew Malin <drewmalin@gmail.com>
2021-03-18 17:07:22 -0700

    support m1, catalina

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,9 +1,15 @@
 class Brev < Formula
   desc "Brev CLI"
   homepage "https://brev.dev"
-  url "https://github.com/brevdev/brev-go-cli/releases/latest/download/brev-osx-64.tar.gz"
-  sha256 "d0411965c431d965b310e1b098c80db581b640585f4f66506bc3e8e1dba6fc20"
+  url "https://github.com/brevdev/brev-go-cli/releases/latest/download" 
   version "0.1.2"
+
+  bottle do
+    root_url "https://github.com/brevdev/brev-go-cli/releases/latest/download"
+    sha256 catalina:      "a17f2ca9870c0b5eb9b8b64f67f48b5578315272d7ee421884a7b17de76adb3b"
+    sha256 big_sur:       "2ff502821e8917e8e893b5f0c88cc7edf584cfcff2a7d5ffba1f362d1b11e6f1"
+    sha256 arm64_big_sur: "c9546911fca4f402a5d25a9f585429296e7f38f9ff3157aaca755ddc4e95d12f"
+  end
 
   def install
     bin.install "brev"
