dafbd355a9eb2c3b6ce1d15780da06f1d8109c4f
Drew Malin <drewmalin@gmail.com>
2021-03-18 18:32:28 -0700

    bottles

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,9 +1,15 @@
 class Brev < Formula
   desc "Brev CLI"
   homepage "https://brev.dev"
-  url "https://github.com/brevdev/brev-go-cli/releases/latest/download/brev-osx-64.tar.gz"
-  sha256 "d0411965c431d965b310e1b098c80db581b640585f4f66506bc3e8e1dba6fc20"
-  version "0.1.2"
+
+#  option "without-completions", "Disable shell completion installation"
+
+  bottle do
+    root_url "https://github.com/brevdev/brev-go-cli/releases/latest/download"
+    sha256 cellar: :any, arm64_big_sur: "e91d819895e76b3c1929ddf90337d1d3f1306ff21fd2def1eeedc1a0b185f358"
+    sha256 cellar: :any, big_sur:       "9e19597dd75dfd9735781a1d9a32bcc34c8d86bd15b783ed5fa8661c5531c7ff"
+    sha256 cellar: :any, catalina:      "cdf27c0f151ac6eea3fb7fe183fe005dd86273fa7cb39112bb35facce064e75a"
+  end
 
   def install
     bin.install "brev"
